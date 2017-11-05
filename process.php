971b6c4c00caab966dae0f580f1cfd06c3be4d98
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-11-05 13:05:23 +0530

    chalne wala version

--- process.php
+++ process.php
@@ -24,13 +24,13 @@ $Password = $_POST['Password'];
 
 //connect to databse
 $conn = mysqli_connect("localhost","root","");   //host,username,password
-mysql_select_db("login");
+mysqli_select_db($conn,"dbslab");
 
 //query the databse for user
 $result = $conn->query("select * from users where Email = '$Email' and DOB ='$Password'") 
                          or die("Failed to query the database!".mysql_error()); //exits current script
 
-$row=$result->mysqli_fetch_assoc();
+$row=mysqli_fetch_assoc($result);
             
 if($row['Email'] == $Email && $row['DOB'] == $Password )
     {
