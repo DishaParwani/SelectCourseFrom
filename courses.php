59d409458f124e1f69446791c91979816b79efc9
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-11-08 11:29:27 +0530

    courses page thoda sa :D

---
+++ courses.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Select Course From</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <link rel="stylesheet"  type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="js/script.s"></script>
+    </head>
+    <body>
+        <?php
+        include ('includes/header.php');
+        ?>
+         <nav class="navbar ">
+            <div class="container">
+                <ul class="nav nav-tabs uppernav">
+                    <li>
+                        <a href="index.php">
+                            Home
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="courses.php">
+                            Courses
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Hostel.php">
+                            Hostel
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Gallery.php">
+                            Gallery
+                        </a>
+                    </li>
+                                        <li>
+                        <a href="ContactUs.php">
+                            Contact Us
+                        </a>
+                    </li>
+                </ul>
+                </div>
+         </nav>
+    </body>
+</html>
