38e1483e19584fa04c7e1fc2d221bd3f15a5a40d
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-11-08 08:47:28 +0530

    after reg page added  Please enter the commit message for your changes. Lines starting

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,13 @@
 <div class="mainheader">
     <div class="headerelement1">
-<img src="images/logo.jpg">
+        <img class="header_img" src="images/logo.jpg">
     </div>
-    
     <div class="headerelement2">
-<h1>SELECT COURSE FROM Pvt Ltd</h1>
+        <h1>SELECT COURSE FROM Pvt Ltd</h1>
     </div>
     <div class="headerelement3">      
-            <a href="login.php"><button class="logsign">Login</button></a>
+        <a href="login.php"><button class="logsign">Login</button></a>
         <a href="#"><button class="logsign">Signup</button></a>
-         </div>
+    </div>
 </div>
 
