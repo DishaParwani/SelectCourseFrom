4833be62b11fffa55f4ccc3302e9a9103a01de2a
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-11-09 04:28:59 +0530

    enroll tables created

---
+++ sql/course_enroll.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 08, 2017 at 10:58 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbslab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_enroll`
+--
+
+CREATE TABLE `course_enroll` (
+  `email` varchar(50) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course_enroll`
+--
+ALTER TABLE `course_enroll`
+  ADD PRIMARY KEY (`email`,`course_id`),
+  ADD KEY `course_id` (`course_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
