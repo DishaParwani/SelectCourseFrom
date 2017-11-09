14441751a4aabd82be057e4345078035fd8256d3
Disha Parwani <disha.parwani9927@gmail.com>
2017-11-09 23:46:19 +0530

    3 routines added

--- sql/ROUTINES.sql
+++ sql/ROUTINES.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 08, 2017 at 01:21 PM
+-- Generation Time: Nov 09, 2017 at 06:12 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -63,4 +63,6 @@ CREATE TEMPORARY TABLE `ROUTINES` (
 --
 
 INSERT INTO `ROUTINES` (`SPECIFIC_NAME`, `ROUTINE_CATALOG`, `ROUTINE_SCHEMA`, `ROUTINE_NAME`, `ROUTINE_TYPE`, `DATA_TYPE`, `CHARACTER_MAXIMUM_LENGTH`, `CHARACTER_OCTET_LENGTH`, `NUMERIC_PRECISION`, `NUMERIC_SCALE`, `CHARACTER_SET_NAME`, `COLLATION_NAME`, `DTD_IDENTIFIER`, `ROUTINE_BODY`, `ROUTINE_DEFINITION`, `EXTERNAL_NAME`, `EXTERNAL_LANGUAGE`, `PARAMETER_STYLE`, `IS_DETERMINISTIC`, `SQL_DATA_ACCESS`, `SQL_PATH`, `SECURITY_TYPE`, `CREATED`, `LAST_ALTERED`, `SQL_MODE`, `ROUTINE_COMMENT`, `DEFINER`, `CHARACTER_SET_CLIENT`, `COLLATION_CONNECTION`, `DATABASE_COLLATION`) VALUES
-('InsertPerson', 'def', 'dbslab', 'InsertPerson', 'PROCEDURE', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'SQL', 'insert into users values(name,email,phone,gender,dob,course)', NULL, NULL, 'SQL', 'NO', 'NO SQL', NULL, 'DEFINER', '2017-11-08 17:35:46', '2017-11-08 17:35:46', '', '', 'root@localhost', 'utf8', 'utf8_general_ci', 'latin1_swedish_ci');
+('InsertPerson', 'def', 'dbslab', 'InsertPerson', 'PROCEDURE', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'SQL', 'insert into users values(name,email,phone,gender,dob,course)', NULL, NULL, 'SQL', 'NO', 'NO SQL', NULL, 'DEFINER', '2017-11-08 17:35:46', '2017-11-08 17:35:46', '', '', 'root@localhost', 'utf8', 'utf8_general_ci', 'latin1_swedish_ci'),
+('Insert_courseenroll', 'def', 'dbslab', 'Insert_courseenroll', 'PROCEDURE', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'SQL', 'insert into course_enroll values(email,course_id)', NULL, NULL, 'SQL', 'NO', 'NO SQL', NULL, 'DEFINER', '2017-11-09 23:41:25', '2017-11-09 23:41:25', '', '', 'root@localhost', 'utf8', 'utf8_general_ci', 'latin1_swedish_ci'),
+('Insert_hostelenroll', 'def', 'dbslab', 'Insert_hostelenroll', 'PROCEDURE', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'SQL', 'insert into hostel_enroll values(email,block_no)', NULL, NULL, 'SQL', 'NO', 'NO SQL', NULL, 'DEFINER', '2017-11-09 23:31:14', '2017-11-09 23:31:14', '', '', 'root@localhost', 'utf8', 'utf8_general_ci', 'latin1_swedish_ci');
