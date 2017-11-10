d2aab112dc04834327f3033887b4a3a03df2349d
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-11-10 12:21:36 +0530

    my profle modified

--- MyProfile.php
+++ MyProfile.php
@@ -24,12 +24,10 @@ and open the template in the editor.
         {
                 $email=$_SESSION['emailid'];
                 $con=  mysqli_connect("localhost", "root", "", "dbslab");
-                $query="select * from registration, courses, hostel, course_enroll, hostel_enroll where email='$email' "
-                        . "and registration.email=course_enroll.email"
-                        . "and course_enroll.course_id=course.course_id"
-                        . "and registration.email=hostel_enroll.email"
-                        . "and hostel_enroll.block_no=hostel.block_no";
+                $query="select distinct name, registration.email, courses.course_id, cname from registration, courses, hostel, course_enroll, hostel_enroll where registration.email='$email' and registration.email=course_enroll.email and course_enroll.course_id=courses.course_id and registration.email=hostel_enroll.email and hostel_enroll.block_no=hostel.block_no;";
+                $query1="select distinct name, registration.email, hostel.block_no, bname from registration, courses, hostel, course_enroll, hostel_enroll where registration.email='$email' and registration.email=course_enroll.email and course_enroll.course_id=courses.course_id and registration.email=hostel_enroll.email and hostel_enroll.block_no=hostel.block_no;";
                 $result=$con->query($query) or exit($con->errno);
+                $result1=$con->query($query1) or exit($con->errno);
         ?>
         <nav class="navbar ">
             <div class="container">
@@ -44,7 +42,7 @@ and open the template in the editor.
                             Courses
                         </a>
                     </li>
-                    <li  class="active">
+                    <li >
                         <a href="Hostel.php">
                             Hostel
                         </a>
@@ -59,11 +57,16 @@ and open the template in the editor.
                             Contact Us
                         </a>
                     </li>
+                     <li class="active">
+                        <a href="MyProfile.php">
+                            My Account
+                        </a>
+                    </li>
                 </ul>
                 </div>
          </nav>
         <div class="container">
-        <div class="scrollable des" style="margin-top: 0px;background-color: #294ca9">
+        <div class=" des" style="margin-top: 0px;background-color: #294ca9">
             <table class="table table-hover table-bordered table-responsive" >
                 <tbody>
                     <tr class="des">
@@ -71,8 +74,6 @@ and open the template in the editor.
                         <th>Email ID</th>
                         <th>Course Id</th>
                         <th>Course Name</th>
-                        <th>Block No.</th>
-                        <th>Block Name</th>
                     </tr>
                     <?php
                     
@@ -84,8 +85,32 @@ and open the template in the editor.
                     <td><?php echo $row["email"]?></td>
                     <td><?php echo $row["course_id"]?></td>
                     <td><?php echo $row["cname"]?></td>
-                    <td><?php echo $row["block_no"]?></td>
-                    <td><?php echo $row["bname"]?></td>
+                    </tr>
+                    <?php
+                        }                   
+        ?> 
+                </tbody>
+            </table>
+            <br>
+            <br><br>
+            <table class="table table-hover table-bordered table-responsive" >
+                <tbody>
+                    <tr class="des">
+                        <th>Name</th>
+                        <th>Email ID</th>
+                        <th>Block No.</th>
+                        <th>Block Name</th>
+                    </tr>
+                    <?php
+                    
+                    while($row1=$result1->fetch_assoc())
+                    {
+                    ?>
+                    <tr>
+                    <td><?php echo $row1["name"]?></td>  
+                    <td><?php echo $row1["email"]?></td>
+                    <td><?php echo $row1["block_no"]?></td>
+                    <td><?php echo $row1["bname"]?></td>
                     </tr>
                     <?php
                         }
